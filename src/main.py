4c6ead3118f9a9ae8cceac9c9ae622f9ba62eef3
takker99 <37929109+takker99@users.noreply.github.com>
2020-08-06 08:18:55 +0900

    :sparkles: 投稿者別に動画のリストを出力できるようにした
    その他の変更：
    - 上位20位のみを取得
    - userIdから投稿者名を取得

--- src/main.py
+++ src/main.py
@@ -1,4 +1,13 @@
 import requests
+from pyquery import PyQuery as pq
+
+def getUserNameFromId(userId):
+    url=f'https://www.nicovideo.jp/user/{str(userId)}'
+    print(f'Access to {url}...')
+    dom = pq(url)
+    result=dom('head').find('meta[property="profile:username"]').attr['content']
+    print(f'Success! userName = {result}')
+    return result
 
 
 def getVideoInfo():
@@ -29,27 +38,33 @@ def getVideoInfo():
             # print(response.json())
             temp += response.json()['data']
     print('Finish loading all data')
-    for j in range(len(temp)):
-        print(f'creator = {temp[j]["userId"]}\ttitile = {temp[j]["title"]}')
 
+    print('Analyzing data...')
+
+    # 投稿者ごとに動画をまとめる
+    # - userIdを投稿者名に変換する
+    # - 上位20人分を取り出す
     result = {temp[i]['userId']: [temp[j]['title'] for j in range(
         len(temp)) if temp[j]['userId'] == temp[i]['userId']]for i in range(len(temp))}
-    sorted_result = sorted(
-        result.items(), key=lambda x: len(x[1]), reverse=True)
+    sorted_result = { getUserNameFromId(item[0]):item[1] for item in sorted(
+        result.items(), key=lambda x: len(x[1]), reverse=True)[:20] }
 
     print('Finish analyzing')
-    for j in range(len(sorted_result)):
+    # 取得結果の確認
+    for key in sorted_result.keys():
         print(
-            f'creator = {sorted_result[j][0]}\tn = {len(sorted_result[j][1])}')
+            f'creator = {key}\tn = {len(sorted_result[key])}')
 
+    print('Writing to the text file...')
     # fileに書き込む
     with open(f'dist/taberungo-list.txt', encoding='utf-8', mode='w') as file:
-        for i in range(len(sorted_result)):
-            file.write(f'[{str(sorted_result[i][0])}]')
+        for key in sorted_result.keys():
+            file.write(f' 投稿者: [{key}]')
             file.write('\n')
             file.write(
-                '\n'.join([f' [{title}]'for title in sorted_result[i][1]]))
+                '\n'.join([f' [{title}]'for title in sorted_result[key]]))
             file.write('\n')
+    print('Successfully finished!')
 
 
 if __name__ == "__main__":
